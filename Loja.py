56d613e70628b2648545f201dea76056b069261b
RODreis2 <kelvyn.kaua9@gmail.com>
2024-07-20 21:30:03 -0300

    terceiro commit

---
+++ Loja.py
@@ -0,0 +1,22 @@
+
+class loja:
+    def __init__():
+        itens = []
+        attacks = []
+       
+
+    def itens(self,name,type, functions):
+        self.name = name
+        self.type = type
+        self.function = functions
+    
+    def attacks(self, name, damage, description):
+        self.name = name
+        self.damage = damage
+        self.description = description
+        description = []
+   
+ 
+ 
+
+       
